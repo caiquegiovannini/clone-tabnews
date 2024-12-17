f1a7c92d6feed68f114c0f9967364fc5e2eacd65
Caique Giovannini <49538313+caiquegiovannini@users.noreply.github.com>
2024-12-16 23:07:24 -0300

    Add scripts `lint:check` and `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto clone do https://www.tabnews.com.br/
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 function Home() {
-    return (
-        <h1>Never give up! Give up never!</h1>
-    )
+  return <h1>Never give up! Give up never!</h1>;
 }
 
-export default Home
+export default Home;
